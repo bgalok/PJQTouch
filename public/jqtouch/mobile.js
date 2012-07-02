ef85929e3a10b67306e7de35a2a3f929509f5248
Brian Galok <brian.galok@gmail.com>
2012-07-01 22:04:46 -0400

    $.ajax

--- public/jqtouch/mobile.js
+++ public/jqtouch/mobile.js
@@ -3,6 +3,7 @@ var jQT = new $.jQTouch({
   //addGlossToIcon: false,
   startupScreen:"apple-touch-startup.png",
   statusBar:'black-translucent',
+  formSelector:'.form',
   preloadImages:[
     '/jqtouch/themes/jqt/img/back_button.png',
     '/jqtouch/themes/jqt/img/back_button_clicked.png',
@@ -34,5 +35,35 @@ jQuery(function() {
     });
     alert("Animating #month " + info.direction);
   });
+  
+	$("#login a.login").tap(function (e) {
+    	var $form = $(this).closest("form");
+    	return app.login($form);
+  	});
+  
+  	$("#login").submit(function (e) {
+   		var $form = $(this);
+    	return app.login($form);
+  	});
+
  	 
 });
+
+
+var app = {
+  login:function ($form) {
+    $.ajax({
+      type:$form.attr("method"), url:$form.attr("action"),
+      dataType:"json", data:$form.serialize(),
+      complete:function (req) {
+        if (req.status === 200 || req.status === 304) {
+          jQT.goBack();
+        } else {
+          alert("There was an error logging in. Try again.");
+        }
+      }
+    });    
+    
+    return false;
+  }
+};
